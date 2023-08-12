3742d78dc73997bfab6f74a79cdd651486050fb2
isdavid92 <isdavid92@hotmail.com>
2023-08-12 08:52:21 -0500

    antes del check

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,9 @@
+const { conn } = require('./DB_connection');
 const server = require('./app');
-const PORT = 3001;
+require('dotenv').config();
+const { PORT } = process.env;
 
-server.listen(PORT, () => {
+server.listen(PORT, async() => {
+    await conn.sync({force:true})
     console.log(`Server raised in port: ${PORT}`);
 });
